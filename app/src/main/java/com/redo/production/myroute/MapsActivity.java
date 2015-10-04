8d64d2e39070c1edbed8be6769cef494eb5c50ee
ReDo <redoprogrammer@gmail.com>
2015-10-04 23:42:59 +0700

    add TimerTask to set timeleft

--- app/src/main/java/com/redo/production/myroute/MapsActivity.java
+++ app/src/main/java/com/redo/production/myroute/MapsActivity.java
@@ -3,14 +3,20 @@ package com.redo.production.myroute;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Environment;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -30,7 +36,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Math.acos;
@@ -55,6 +67,9 @@ public class MapsActivity extends FragmentActivity{
 
 
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,28 +80,56 @@ public class MapsActivity extends FragmentActivity{
         txvTimeLeft =(TextView) findViewById(R.id.txvTimeLeft);
 
 
-        final CounterClass timer = new CounterClass(120000,1000);
-        timer.start();
 
 
-        //Thread thread = new Thread(new Timer());
-        //thread.start();
 
-        //check GPS is enable or not
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            Toast.makeText(this,"GPS is enabled",Toast.LENGTH_SHORT).show();
+        //*******************************************************
+        //checking GPS and Network available
+        //*******************************************************
+        ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        //mobile
+        NetworkInfo.State mobile = conMan.getNetworkInfo(1).getState();
+        //wifi
+        NetworkInfo.State wifi = conMan.getNetworkInfo(1).getState();
+
+
+        if (mobile == NetworkInfo.State.CONNECTED || mobile == NetworkInfo.State.CONNECTING)
+        {
+            Toast.makeText(this,"Network connected",Toast.LENGTH_LONG).show();
         }
-        else {
-            showDisabledGPSAlert();
+        else if (wifi == NetworkInfo.State.CONNECTED || wifi == NetworkInfo.State.CONNECTING)
+        {
+            Toast.makeText(this,"Wifi connected",Toast.LENGTH_LONG).show();
         }
+
+
+
+        //Time counting
+
+        Timer T=new Timer();
+        T.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        txvTimeLeft.setText(setClock(count));
+                        count++;
+                    }
+                });
+            }
+        }, 1000, 1000);
 
 
     }
 
+
+
     private void showDisabledGPSAlert() {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage("GPS is disable. Would you like to eable it?")
+        alertDialog.setMessage("GPS is disable. Would you like to enable it?")
                 .setCancelable(false)
                 .setPositiveButton("Go to setting",
                         new DialogInterface.OnClickListener() {
@@ -102,6 +145,7 @@ public class MapsActivity extends FragmentActivity{
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
+                return;
             }
         });
         AlertDialog alert = alertDialog.create();
@@ -116,29 +160,7 @@ public class MapsActivity extends FragmentActivity{
     }
 
 
-    public  void onReset(View v)    {
-        mMap.clear();
-        distance = 0;
-        txvDistance.setText(Long.toString(distance));
 
-        currentPoint = null;
-        txvSpeed.setText("0asf");
-        txvTimeLeft.setText("aba");
-    }
-
-
-    public double getDistance(LatLng fP, LatLng lP)    {
-        double l1 = toRadians(fP.latitude);
-        double l2 = toRadians(lP.latitude);
-        double g1 = toRadians(fP.longitude);
-        double g2 = toRadians(lP.longitude);
-
-        double dist = acos(Math.sin(l1) * Math.sin(l2) +  Math.cos(l1) *  Math.cos(l2) *  Math.cos(g1 - g2));
-        if(dist < 0) {
-            dist = dist + Math.PI;
-        }
-        return Math.round(dist * 6378100);
-    }
 
 
 
@@ -208,51 +230,84 @@ public class MapsActivity extends FragmentActivity{
     }
 
 
-    public class Timer implements Runnable
-    {
 
-        @Override
-        public void run() {
-            count++;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            txvTimeLeft.setText(Long.toString(count));
+    //capture screen
+    public void onSave(View v){
+
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(this,"Successful",Toast.LENGTH_LONG).show();
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+
+            Toast.makeText(this,"Fail",Toast.LENGTH_LONG).show();
         }
+
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+
+    public String setClock(long n){
+        int second = (int)n%60;
+        n = n/60;
+        int minute = (int)n%60;
+
+        int hour = (int)n/60;
+        String strSecond = String.valueOf(second);
+        String strMinute = String.valueOf(minute);
+        String strHour = String.valueOf(hour);
+        if(second<10) strSecond = "0" + strSecond;
+        if(minute<10) strMinute = "0" + strMinute;
+        if(hour<10) strHour = "0"+strHour;
+        return strHour+":"+strMinute+":"+strSecond;
+    }
+    public  void onReset(View v)    {
+        mMap.clear();
+        distance = 0;
+        count = 0;
+        txvDistance.setText(Long.toString(distance));
+
+        currentPoint = null;
+
     }
 
 
+    public double getDistance(LatLng fP, LatLng lP)    {
+        double l1 = toRadians(fP.latitude);
+        double l2 = toRadians(lP.latitude);
+        double g1 = toRadians(fP.longitude);
+        double g2 = toRadians(lP.longitude);
 
-    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-    @SuppressLint("NewApi")
-    public class CounterClass extends CountDownTimer    {
-
-        public CounterClass(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
+        double dist = acos(Math.sin(l1) * Math.sin(l2) +  Math.cos(l1) *  Math.cos(l2) *  Math.cos(g1 - g2));
+        if(dist < 0) {
+            dist = dist + Math.PI;
         }
-
-
-        @SuppressLint("NewApi")
-        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-
-        @Override
-        public void onTick(long millis) {
-           String hms = String.format("%02d:%02d:%02d",
-                    TimeUnit.MILLISECONDS.toHours(millis),
-                    TimeUnit.MILLISECONDS.toMinutes(millis) -
-                    TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
-                    TimeUnit.MILLISECONDS.toSeconds(millis) -
-                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-            txvTimeLeft.setText(hms);
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
+        return Math.round(dist * 6378100);
     }
-
 
 }
